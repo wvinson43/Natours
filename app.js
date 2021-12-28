0059917a02d96efbc9d743a37500894be7b85041
W Vinson <wesleyvinson2016@yahoo.com>
2021-12-28 08:24:04 -0600

    misspell

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-const cors = required('cors');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
