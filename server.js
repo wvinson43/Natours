6c491edcfe21f85152349a4ee22c8a36e44dcd64
W Vinson <wesleyvinson2016@yahoo.com>
2021-12-27 23:04:07 -0600

    maybe

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
