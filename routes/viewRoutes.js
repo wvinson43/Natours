fca2c507df40d012783c4db1b93b88cdefd6e4c6
W Vinson <wesleyvinson2016@yahoo.com>
2021-12-29 01:10:12 -0600

    final hopeful

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.use(viewsController.alert);
+router.use(viewsController.alerts);
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
 
